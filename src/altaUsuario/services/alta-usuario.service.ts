b3e24272e82d6e0873564bee5f19ce6fef099b7f
defrentemall <choloman14@gmail.com>
2023-02-24 02:12:56 -0300

    main

--- src/altaUsuario/services/alta-usuario.service.ts
+++ src/altaUsuario/services/alta-usuario.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { actualizar_usuarioDto } from '../dto/actualizarusuario.dto';
 import { altaUsuarioDTO } from '../dto/altausuario.dto';
 import { altaUsuarioEntity } from '../entitys/altaUsuario.entity';
 
@@ -10,21 +11,49 @@ export class AltaUsuarioService {
 
     constructor(@InjectRepository(altaUsuarioEntity) private altaUsuarioRepo: Repository<altaUsuarioEntity>) { }
 
-    crearAlta(alta: altaUsuarioDTO) {
+    async crearAlta(alta: altaUsuarioDTO) {
+
+        const userexistente= await this.altaUsuarioRepo.findOne({
+            where: {
+                dni: alta.dni
+            }
+        })
+        if(userexistente){
+            return new HttpException('DNI existente, intenta con otro dni', HttpStatus.CONFLICT)
+        }
+
         const newAlta = this.altaUsuarioRepo.create(alta)
         return this.altaUsuarioRepo.save(newAlta)
     }
 
     getListarUsuarios(){
-        return this.altaUsuarioRepo.find()
+        return this.altaUsuarioRepo.find();
     }
 
-    getbuscarusuario(dni:number){
-        return this.altaUsuarioRepo.findOne({
+    async getbuscarusuario(id:number){
+        const usuarioexiste= await this.altaUsuarioRepo.findOne({
             where: {
-                dni
-            }
-        })
+                id,
+            },
+        });
+        if (!usuarioexiste){
+            return new HttpException('DNI no encontrado o no existe', HttpStatus.NOT_FOUND)
+        }
+        return usuarioexiste;
+    }
+
+    async deleteelimnarusuario(id:number) {
+        const usuarioEliminado= await this.altaUsuarioRepo.delete({id});
+
+        if(usuarioEliminado.affected === 0 ){
+            return new HttpException('El usuario no existe y no puede ser eliminado', HttpStatus.NOT_FOUND)
+        }
+        return usuarioEliminado;
+    }
+
+
+    updateUsuario(id: number, user:actualizar_usuarioDto){
+        return this.altaUsuarioRepo.update({id}, user)
 
     }
 
