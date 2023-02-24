7fde66a75f05b4ab4e3ab7f063b665976dee4a53
defrentemall <choloman14@gmail.com>
2023-02-24 02:13:09 -0300

    main

--- src/altaUsuario/controller/alta-usuarios.controller.ts
+++ src/altaUsuario/controller/alta-usuarios.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseIntPipe, Delete, Patch,} from '@nestjs/common';
+import { actualizar_usuarioDto } from '../dto/actualizarusuario.dto';
 import { altaUsuarioDTO } from '../dto/altausuario.dto';
 import { AltaUsuarioService } from '../services/alta-usuario.service';
 
@@ -15,14 +16,30 @@ export class AltaUsuariosController {
     }
 
     @Get(':id') /*mostra*/
-    getbuscarusuraio(@Param('id', ParseIntPipe) id:number): Promise <altaUsuarioDTO>{
+    getbuscarusuraio(@Param('id', ParseIntPipe) id:number){
         return this.altaService.getbuscarusuario(id);
     }
 
     @Post()
-    createAlta(@Body() newAlta: altaUsuarioDTO): Promise<altaUsuarioDTO> {
+    createAlta(@Body() newAlta: altaUsuarioDTO) {
         return this.altaService.crearAlta(newAlta)
 
     }
+    
+    @Delete(':id')
+    deleteelimnarusuario(@Param('id', ParseIntPipe) id:number){
+    return this.altaService.deleteelimnarusuario(id);
+
+    }
+
+@Patch(':id')
+update_Usuarios(@Param('id', ParseIntPipe) id: number, @Body() user:actualizar_usuarioDto){
+return this.altaService.updateUsuario(id, user)
+
+}
+
+
+
+
 
 }
---
+++ src/altaUsuario/dto/actualizarusuario.dto.ts
@@ -0,0 +1,13 @@
+export class actualizar_usuarioDto {
+
+    dni?: number
+
+    contraseña?: string
+
+    rol?: string
+
+    lider?: string
+
+    jefe?: string
+
+}
